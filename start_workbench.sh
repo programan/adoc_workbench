ce2fc45357adb974dd00568808359010d54d58cd
programan <github@programan.com>
2020-06-01 22:29:47 +0900

    Separated ENTRYPOINT processing for Linux and other OS.

--- start_workbench.sh
+++ start_workbench.sh
@@ -2,5 +2,11 @@
 
 set -x
 
-# docker-compose run --rm app bash
-docker-compose run --rm -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) app bash
+case "$OSTYPE" in
+  linux*)
+    docker-compose run --rm -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) app bash
+    ;;
+  *)
+    docker-compose run --rm app bash
+    ;;
+esac
